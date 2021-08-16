c6eed96a6a8bf59d786bc54f8992ff0c63673b11
hazel-u <hyeseung0124@gmail.com>
2021-08-16 22:36:05 +0900

    cookie path

--- JSPProject/src/com/newlecture/web/Calc2.java
+++ JSPProject/src/com/newlecture/web/Calc2.java
@@ -69,6 +69,8 @@ public class Calc2 extends HttpServlet {
 			
 			Cookie valueCookie = new Cookie("value", String.valueOf(v));
 			Cookie opCookie = new Cookie("op", op);
+			valueCookie.setPath("/calc2");
+			opCookie.setPath("/calc2");
 			response.addCookie(valueCookie);
 			response.addCookie(opCookie);
 		}
