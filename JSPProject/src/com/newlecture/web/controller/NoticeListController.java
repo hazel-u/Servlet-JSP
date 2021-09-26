64d5307d2235dd383c2b1e1cfb76cd44214e5708
hazel-u <hyeseung0124@gmail.com>
2021-09-26 15:44:06 +0900

    목록에서 페이징 시 데이터 유지

--- JSPProject/src/com/newlecture/web/controller/NoticeListController.java
+++ JSPProject/src/com/newlecture/web/controller/NoticeListController.java
@@ -24,21 +24,25 @@ public class NoticeListController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 컨트롤러 : 사용자가 요청한 내용을 받는 역할
-		// 사용자가 검색을 할 때 들어오는 내용 : list?f=title&q=a (f는 field, q는 검색 내용)
+		// 사용자가 검색을 할 때 들어오는 내용 : list?p=1&f=title&q=a (p는 page, f는 field, q는 검색 내용)
 		
 		// 옵션으로 오는 값을 사용할 때는 null이 올수도 있기 때문에 사전에 미리 체크를 해주어야한다.
 		// 방법 : 임시변수에 값을 담아두고 null 체크 후 정해진 변수에 담는다.
+		String page_ = request.getParameter("p"); // int형일 경우 null 체크를 못하기 때문에 String으로 받고 나중에 형변환을 해준다. (Integer로 할경우에는 나중에 형변환을 못하기 때문에 불가능)
 		String field_ = request.getParameter("f");
 		String query_ = request.getParameter("q");
+		
+		int page=1;
 		String field = "title"; // 기본값 설정
 		String query="";
-		if(field_ != null) field = field_; // null이 아닐 경우에만 field에 넣기
-		if(query_ != null) query = query_;
+		if(page_ != null && !page_.equals("")) page = Integer.parseInt(page_);
+		if(field_ != null && !field_.equals("")) field = field_; // null이 아닐 경우에만 field에 넣기
+		if(query_ != null && !query_.equals("")) query = query_;
 		
 		
 		NoticeService service = new NoticeService();
 		
-		List<Notice> list = service.getNoticeList(field, query, 1);
+		List<Notice> list = service.getNoticeList(field, query, page);
 		
 		
 		request.setAttribute("list", list);
